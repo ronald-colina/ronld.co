b9b6807051b5cf9916b2b6f05898d587d45b5500
ronald-colina <ronldco@gmail.com>
2022-01-13 16:59:33 -0300

    ajustando detalles finales

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-const header = document.querySelector ('main-header');
+const header = document.querySelector('main-header');
 
 window.addEventListener('scroll', () => {
     const scrollPos = window.scrollY;
-    if(scrollPos > 10) {
+    if(scrollPos > 10){
         header.classList.add('scrolled');
-    } else {
+    } 
+    else {
         header.classList.remove('scrolled')
     }
 })
